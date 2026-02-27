89dcf17faac0233b1f887e6529bfab90e2586cc2
baoquyentran2101-maker <baoquyen.tran2101@gmail.com>
2026-02-27 12:48:32 +0700

    Fix import path for supabaseClient

--- bq_cafe_pos_full/app/table/[id]/page.js
+++ bq_cafe_pos_full/app/table/[id]/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../../lib/supabaseClient';
 
 const fmtVND = (n) => Number(n || 0).toLocaleString('vi-VN') + ' đ';
 
